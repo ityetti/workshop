88ebd801734247bb1432ed9873ad6be559cce11a
Oleksii Borovyk <ityetti@gmail.com>
2020-10-06 19:25:31 +0300

    Lesson 16

--- view/frontend/web/js/view/todo.js
+++ view/frontend/web/js/view/todo.js
@@ -1,5 +1,12 @@
 define(['uiComponent'], function (Component) {
     'use strict';
 
-    return Component.extend({});
+    return Component.extend({
+        tasks: [
+            {label: "Task 1"},
+            {label: "Task 2"},
+            {label: "Task 3"},
+            {label: "Task 4"},
+        ]
+    });
 });
