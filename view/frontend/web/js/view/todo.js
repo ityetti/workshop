ab5f74c833e591af1ae57afe53899cd68cf617d0
Oleksii Borovyk <ityetti@gmail.com>
2020-10-06 20:09:36 +0300

    Lesson 17

--- view/frontend/web/js/view/todo.js
+++ view/frontend/web/js/view/todo.js
@@ -2,11 +2,19 @@ define(['uiComponent'], function (Component) {
     'use strict';
 
     return Component.extend({
-        tasks: [
-            {label: "Task 1"},
-            {label: "Task 2"},
-            {label: "Task 3"},
-            {label: "Task 4"},
-        ]
+        defaults: {
+            tasks: [
+                {label: "Task 1"},
+                {label: "Task 2"},
+                {label: "Task 3"},
+                {label: "Task 4"},
+            ]
+        },
+
+        initObservable: function() {
+            this._super().observe(['tasks']);
+
+            return this;
+        },
     });
 });
