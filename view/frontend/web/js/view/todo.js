79113b69ec0c394c72e7da07e87304468938ebc0
Oleksii Borovyk <ityetti@gmail.com>
2020-10-07 21:00:45 +0300

    Lesson 21

--- view/frontend/web/js/view/todo.js
+++ view/frontend/web/js/view/todo.js
@@ -1,13 +1,16 @@
-define(['uiComponent'], function (Component) {
+define([
+    'uiComponent',
+    'jquery'
+], function (Component, $) {
     'use strict';
 
     return Component.extend({
         defaults: {
             tasks: [
-                {label: "Task 1"},
-                {label: "Task 2"},
-                {label: "Task 3"},
-                {label: "Task 4"},
+                {id: 1, label: "Task 1", status: false},
+                {id: 2, label: "Task 2", status: false},
+                {id: 3, label: "Task 3", status: false},
+                {id: 4, label: "Task 4", status: true},
             ]
         },
 
@@ -16,5 +19,19 @@ define(['uiComponent'], function (Component) {
 
             return this;
         },
+
+        switchStatus: function (data, event) {
+            const taskId = $(event.target).data('id');
+            
+            var items = this.tasks().map(function (task) {
+                if (task.id === taskId) {
+                    task.status = !task.status;
+                }
+
+                return task;
+            });
+
+            this.tasks(items);
+        }
     });
 });
