f5444982a74ff55301e7daeff63511c2dfb4a076
Oleksii Borovyk <ityetti@gmail.com>
2020-10-17 23:28:13 +0300

    Lesson 28

--- view/frontend/web/js/view/todo.js
+++ view/frontend/web/js/view/todo.js
@@ -8,6 +8,7 @@ define([
     return Component.extend({
         defaults: {
             newTaskLabel: '',
+            buttonSelector: '#add-new-task-button',
             tasks: [
                 {id: 1, label: "Task 1", status: false},
                 {id: 2, label: "Task 2", status: false},
@@ -68,6 +69,13 @@ define([
                 status: false
             });
             this.newTaskLabel('');
+        },
+
+        checkKey: function() {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                $(this.buttonSelector).click();
+            }
         },
     });
 });
